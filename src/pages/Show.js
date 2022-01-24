376dda72177c4ee49089a597653b386e4b647f93
Max Ma <maxxma97@gmail.com>
2022-01-24 15:32:29 -0500

    wip: rendering name and background image on show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,12 +1,56 @@
-function Show(props){
+import { useState } from "react";
 
-    const id = props.match.params.id; // .match gives access to URL params
+function Show(props){
+    const id = props.match.params.id; // .match gives access to URL param
     const background = props.backgrounds.find(background => background._id === id);
+
+    const [editForm, setEditForm] = useState(background);
+
+    const handleChange = (event) => {
+        setEditForm({
+            ...editForm, // spread out params of the editForm
+            [event.target.name]: event.target.value
+        })
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.updateABackground(editForm, id);
+        props.history.push("/"); // .push allows adding of elements to history, redirects user to "/"
+    };
+
+    const removeBackground = () => {
+        props.deleteABackground(id);
+        props.history.push("/")
+    }
 
     return(
         <div className="background">
             <h1>{background.name}</h1>
-            <img src={background.url} alt={background.name} />
+            {
+                background.url && <img src={background.url} alt={background.name} /> // img tag won't render unless background has a URL
+            }   {/* operand right of && won't execute unless the left operand evaluates to truthy*/}
+            <button id="delete" onClick={removeBackground}>
+                Delete Background
+            </button>
+
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    value={editForm.name}
+                    name="name"
+                    placeholder="Name"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    value={editForm.url}
+                    name="url"
+                    placeholder="background URL"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Update Background" />
+            </form>
         </div>
     )
 };
