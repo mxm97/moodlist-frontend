59e27750a1d2e3a8643726fc593576e39cb1febe
Max Ma <maxxma97@gmail.com>
2022-01-26 19:12:49 -0500

    fixed issue where play button would start audio on second click

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "../styles/AudioPlayer.css";
 import { BsArrowLeftShort } from "react-icons/bs";
 import { BsArrowRightShort } from "react-icons/bs";
@@ -7,15 +7,28 @@ import { FaPause } from "react-icons/fa";
 
 function AudioPlayer(props) {
 
+    // State
     const [isPlaying, setIsPlaying] = useState(false); // set useState to false initially b/c we don't want audio to play automatically on page load
 
+    // References
+    const audioPlayer = useRef(); // reference our audio component
+
     const togglePlayPause = () => {
-        setIsPlaying(!isPlaying); // toggles the isPlaying state to change which button icon is displayed
+
+        const prevValue = isPlaying;
+
+        setIsPlaying(!prevValue); // toggles the isPlaying state to change which button icon is displayed
+        if (!prevValue) {
+            audioPlayer.current.play();
+        } else {
+            audioPlayer.current.pause();
+        }
     }
 
     return(
         <div className="audioPlayer">
-            <audio src="https://soundcloud.com/discolithe/maria-takeuchi-plastic-love" preload="metadata"></audio>
+            {/* Audio play works, just need to find a good place to host my audio file */}
+            <audio ref={audioPlayer} src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3" preload="metadata"></audio>
             <button className="forwardBackward">
                 <BsArrowLeftShort /> 30
             </button>
